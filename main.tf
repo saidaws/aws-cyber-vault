f466c512b1db04645a522f36ce4bd706b5d99d86
Said Benallal <saidben@amazon.com>
2024-12-18 08:41:04 -0500

    test deploy wf

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "cyber_recovery_lag_vault" {
   source                  = "./modules/cyber-recovery-lag-vault"
+  environment             = var.environment
   app_name                = var.app_name
   cyberapp_principals     = var.cyberapp_principals
   max_retention_days      = var.max_retention_days
