9761896e9c4be24e8096d0782d3b8a5c3f33e816
Said Benallal <saidben@amazon.com>
2024-12-18 09:01:59 -0500

    update locals.tf

--- modules/cyber-recovery-lag-vault/locals.tf
+++ modules/cyber-recovery-lag-vault/locals.tf
@@ -1,9 +1,7 @@
-locals {
-  # account_id = data.aws_caller_identity.this.account_id
-  # region = data.aws_region.this.name
-
-  # tags = {
-  #   environment = var.environment
-  #   GitRepo     = "https://github.com/ExxonMobil/backup-lag-vault"
-  # }
-}
+# locals {
+#   tags = {
+#     app_name    = var.app_name
+#     environment = var.environment
+#     GitRepo     = "https://github.com/EMOrg-Prd/aws-cyber-vault"
+#   }
+# }
