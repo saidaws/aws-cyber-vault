ad2a4d17a6555d48d42f54207ddd2c1073bd4452
Said Benallal <saidben@amazon.com>
2024-12-19 08:35:29 -0500

    update GitRepo URL in locals.tf

--- modules/cyber-recovery-lag-vault/locals.tf
+++ modules/cyber-recovery-lag-vault/locals.tf
@@ -5,6 +5,6 @@ locals {
   # tags = {
   #   app_name    = var.app_name
   #   environment = var.environment
-  #   GitRepo     = "https://github.com/ExxonMobil/backup-lag-vault"
+  #   GitRepo     = "https://github.com/EMOrg-Prd/aws-cyber-vault"
   # }
 }
